3cdd20b437fa4d06e2498f6b4af6077077e9473b
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-12-06 12:37:23 -0500

    Remove console.log

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -3,10 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-bar"
 export default class extends Controller {
   search() {
-    // clearTimeout(this.timeout)
-    // this.timeout = setTimeout(() => {
-    //   this.element.submit()
-    // }, 200)
-    console.log("searching");
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.submitRequest()
+    }, 200)
   }
 }
