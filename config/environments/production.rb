b125b9bc5e2668a2b869269ed30f0f7f158675a8
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-12-06 15:02:35 -0500

    Add action mailer authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "helpjuice_search_production"
 
+    # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://rails-duq1.onrender.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'mstanley.me@gmail.com',
+    password:             'qmcspfuwrlvjfqhr',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
