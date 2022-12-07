3c5b2f40a4381fbe2d7a35fcb0053dfdb8f87ea9
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-12-07 11:37:00 -0500

    Add change to host link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://rails-duq1.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'https://rails-sq95.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
